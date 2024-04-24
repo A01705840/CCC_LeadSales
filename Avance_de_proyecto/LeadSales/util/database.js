74d547408cbb5882ef732b9ec7ad7b9ff786ef7e
Maria José Soto <A01705840@tec.mx>
2024-04-23 19:32:25 -0600

    PRUEBA - For en analitica para ver si funciona

--- Avance_de_proyecto/LeadSales/util/database.js
+++ Avance_de_proyecto/LeadSales/util/database.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'majo',
+    host: 'localhost',
+    user: 'root',
     database: 'leadsalesdb3',
-    password: 'oto',
+    password: '',
 });
 module.exports = pool.promise();
