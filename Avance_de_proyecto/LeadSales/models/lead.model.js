2a477447c40650eaf17fefc4d290b8ab8467070c
Mauricio Salas Hernández <A01710309@tec.mx>
2024-04-24 07:10:45 -0600

    cambios

--- Avance_de_proyecto/LeadSales/models/lead.model.js
+++ Avance_de_proyecto/LeadSales/models/lead.model.js
@@ -55,7 +55,7 @@ module.exports = class Lead {
         const endDate = new Date(2023, 0, 1); 
         let startDate = new Date(endDate); // Crea una copia de endDate
         let groupBy;
-    
+
         switch (parseInt(range)) {
             case 1:
                 startDate.setDate(endDate.getDate() - 7); // Una semana antes de la fecha actual
