8936b46d7252344070b50aaf45389ae33ca6000a
Galo del Rio <A01710791@tec.mx>
2024-04-25 13:29:28 -0600

    galo-loggin-1

--- Avance_de_proyecto/LeadSales/routes/usuarios.routes.js
+++ Avance_de_proyecto/LeadSales/routes/usuarios.routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const usuariosController= require('../controllers/usuarios.controller');
+const isAuth = require('../util/isAuth');
 
 
-router.get('/login', usuariosController.get_login);
+
+router.get('/login',  usuariosController.get_login);
 
 router.get('/signup', usuariosController.get_signup);
 
-router.post('/signup', usuariosController.post_signup);
+router.post('/signup',  usuariosController.post_signup);
+
+router.post('/login', usuariosController.post_login);
+
+router.get('/logout', usuariosController.get_logout);
+
+router.get('/', isAuth, usuariosController.get_logout);
 
 module.exports = router;
