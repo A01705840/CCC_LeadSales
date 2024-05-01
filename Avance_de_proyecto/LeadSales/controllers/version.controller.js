5df2484dbd91dc9f128866b6e2ce6b992d53712e
Maria José Soto <A01705840@tec.mx>
2024-05-01 15:01:19 -0600

    Revert "FIX - Telefonos null"
    This reverts commit 4919973794308a08d3e6802f06eafa6ead283c2c.

--- Avance_de_proyecto/LeadSales/controllers/version.controller.js
+++ Avance_de_proyecto/LeadSales/controllers/version.controller.js
@@ -115,11 +115,11 @@ exports.post_historial = async (req, res, next) => {
                         }
                         if (rowData[posiciones[1]]==''){
                             rowData[posiciones[1]]=null;
-                        } else{
-                            rowData[posiciones[1]]= rowData[posiciones[1]].split(' ').join('');
+                        }
+                        if (typeof rowData[posiciones[1]] == "string") {
+                            rowData[posiciones[1]]= null;
                         }
                         rowData[posiciones[3]] = convertirFecha(rowData[posiciones[3]]);
-                        console.log(rowData[posiciones[1]]);
                         // Ejecutar ambas consultas y esperar a que se completen
                         const [usuarioResult, leadResult] =await Promise.all([
                             Usuario.guardar_nuevo(rowData[posiciones[0]]),
